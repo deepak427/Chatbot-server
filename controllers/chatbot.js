76c761d26c2109bb238603ed5718ec2ea44f6ec6
Mayank <mayank8630bisht@gmail.com>
2023-11-04 19:06:22 +0530

    added more data to chatbot

--- controllers/chatbot.js
+++ controllers/chatbot.js
@@ -18,15 +18,11 @@ export const chatbot = async (req, res) => {
     Governor's Profile
     Lieutenant General Gurmit Singh, PVSM, UYSM, AVSM, VSM (Retired) 
     Former Deputy Chief of Army Staff
-    
     Superannuation: 31st January 2016
-    
     Date of Birth: 1st  February 1956
-    
     Birth Place: Jalal Usman, Amritsar, Punjab
-    
     Primary Education: Sainik School Kapurthala, Punjab.
-    
+  
     Lieutenant General Gurmit Singh, who is known for his military deftness and proficiency in the Indian Army, is presently bedecking the designation of Governor of Uttarakhand.  Shri Singh’s personality is a combination of bravery, courage, kindness, compassion, simplicity and honesty.  A wonderful amalgamation of simplicity and popularity in his personality.  An expert in taking tough decisions in his military career, Shri Singh is broad-minded, soft in spirit, heart and thoughts.  He has compassion, love and affection for all.
     
     Forgetting caste, religion and class, he has the attitude to see only soul and Almighty in a person.  Shri Gurmit Singh Ji, who has entirely imbibed the three virtues of Sikhism; kindness, simplicity and honesty, in his personal life, has a unique combination of military aptitude and pleasing persona.  He touches everyone’s heart closely.  After becoming the Governor of Uttarakhand, the spirit of Patriotism and his Vision of serving the State have already been felt in his first message.
@@ -48,6 +44,59 @@ export const chatbot = async (req, res) => {
     He has special attachment with soldiers, warriors and the martyrs.  He was also Chief Executive Patron of the Rashtriya Sainik Sanstha . He has been participating in TV Media on Nation First focus, as Jai Hind General and as a defense expert.
     
     He has been awarded with Four Presidential Awards and two  commendations by Chief of Army Staff, during his nearly 40 years of service in the Army;  Param Vishisht Seva Medal, Uttam Yudh Seva Medal, Ati Vishisht Seva Medal and Vishisht Seva Medal.  He had also served in Banbasa, of Uttarakhand during his tenure in Army.
+
+   The primary objectives of the Governor of Uttarakhand are to:
+    Uphold the Constitution of India and ensure the smooth functioning of the state government.
+    Act as a bridge between the state government and the central government.
+    Protect the interests of the people of Uttarakhand.
+    Promote the development of Uttarakhand.
+
+    As Governor of Uttarakhand, Lt. Gen. Gurmit Singh has made a number of significant achievements. Here is a summary of his major achievements in the following areas:
+
+  Education
+
+    Launched the "Uttarakhand Gaurav Abhiyan" to promote education in the state. The Abhiyan aims to improve the quality of education in     government schools, increase the enrollment rate of children, and provide scholarships to deserving students.
+    Started the "Uttarakhand Bal Vidyalaya Yojana" to provide affordable and quality education to children from underprivileged families. The     Yojana provides free education, uniforms, and books to children from Classes 1 to 8.
+    Set up the "Uttarakhand Rashtriya Sanskrit Sansthan" to promote Sanskrit education and culture. The Sansthan offers courses in Sanskrit     language, literature, and culture at the undergraduate, postgraduate, and doctoral levels.
+    Inaugurated the "Uttarakhand Skill Development University" to provide skill development training to youth in the state. The University     offers courses in a variety of trades, including engineering, IT, healthcare, and hospitality.
+    Healthcare
+    
+    Launched the "Uttarakhand Swasthya Yojana" to provide affordable and quality healthcare to all citizens of the state. The Yojana provides     free medical treatment to patients in government hospitals and subsidized treatment in private hospitals.
+    Started the "Uttarakhand Janani Suraksha Yojana" to improve maternal and child health in the state. The Yojana provides free prenatal and     postnatal care to pregnant women and their babies.
+    Set up the "Uttarakhand Ayushman Bharat Pradhan Mantri Jan Arogya Yojana" to provide financial assistance to poor and vulnerable families     for medical treatment. The Yojana covers a wide range of medical expenses, including hospitalization, surgery, and diagnostics.
+    Inaugurated the "Uttarakhand Medical Education and Research University" to improve medical education and research in the state. The     University offers courses in medicine, dentistry, and nursing.
+    Agriculture
+    
+    Launched the "Uttarakhand Krishi Utpadan Yojana" to increase agricultural production and income in the state. The Yojana provides subsidies     to farmers for seeds, fertilizers, and pesticides.
+    Started the "Uttarakhand Horticulture Mission" to promote horticulture in the state. The Mission provides financial assistance to farmers     for planting fruit trees and vegetables.
+    Set up the "Uttarakhand Organic Agriculture Mission" to promote organic farming in the state. The Mission provides training and support to     farmers who want to switch to organic farming.
+    Inaugurated the "Uttarakhand Agriculture and Veterinary University" to improve agricultural education and research in the state. The     University offers courses in agriculture, horticulture, and veterinary sciences.
+    
+  Infrastructure
+
+   Launched the "Uttarakhand Har Ghar Jal Yojana" to provide drinking water to every household in the state. The Yojana aims to complete the    piped water supply project in all villages of Uttarakhand by 2024.
+   Started the "Uttarakhand Sadak Yojana" to improve road connectivity in the state. The Yojana aims to construct and repair a total of 10,000    km of roads in Uttarakhand by 2025.
+   Set up the "Uttarakhand Smart City Mission" to develop smart cities in Uttarakhand. The Mission aims to develop Dehradun, Rishikesh, and    Haridwar as smart cities.
+   Inaugurated the "Uttarakhand Airport Authority of India" to develop and manage airports in Uttarakhand. The Authority is responsible for the    development and operation of Jolly Grant Airport in Dehradun and Pantnagar Airport in Udham Singh Nagar.
+   Environment
+   
+   Launched the "Uttarakhand Van Mahotsav" to promote tree plantation and forest conservation in the state. The Mahotsav aims to plant 10    million trees in Uttarakhand by 2025.
+   Started the "Uttarakhand Ganga Cleaning Mission" to clean the Ganga River and its tributaries. The Mission is responsible for the    construction and operation of sewage treatment plants, river banks
+
+   The State of Uttarakhand (earlier known as Uttaranchal) came into existence on 9th November, 2000 as the 27th State of the Republic of India. With the creation of the State of Uttarakhand, the Raj Bhawan was temporarily established at Bijapur House situated on New Cantonment Road, Dehradun. Subsequently the Circuit House, Dehradun was redesignated as Raj Bhawan and the first Governor, Shri Surjit Singh Barnala, shifted there on 25th December 2000. The present Raj Bhawan (earlier known as Circuit House) was built in 1902. It was then called “Court House”, where the then British Governor of United Provinces (now Uttar Pradesh) often used to reside. In the post-Independence period, India’s first Prime Minister, Pt. Jawaharlal Nehru used to stay in this building whenever he visited Dehradun. From time to time, various Presidents of India and almost all Prime Ministers, so far, have stayed in this historic building. The Rajbhawan is situated at a height of 2305 ft. from sea level.
+
+
+While the Raj Bhawan was shifted to Circuit House, the Governor’s Secretariat continued to function from Bijapur House premises till 27 July 2009, when the new Raj Bhawan Secretariat and Auditorium buildings were inaugurated by H.E. Shri B. L. Joshi (the 3rd Governor of Uttarakhand). Later, a separate Governor’s Official Residence was built in this premises which was inaugurated by Smt. Margaret Alva (the fourth Governor of Uttarakhand) on 14 April, 2010. The Old building, previously known as Circuit House, is now being used as Raj Bhawan Guest House.
+
+The sprawling Lawns, Bonsai Garden and rich floral species add to the charm and beauty of the Rajbhawan.
+
+The Auditorium of Raj Bhawan is a special venue where various important events e.g. oath-taking ceremonies, seminars, book-release functions and cultural programmes etc. are organised.
+
+Uttarakhand is one of the few States in the country which can boast of two Raj Bhawans. The second Raj Bhawan of Uttarakhand is situated in Nainital. In the pre-Independence era, Nainital served as the summer capital of United Provinces (now Uttar Pradesh) and this beautiful building, built like a Scottish castle was christened as the “Government House”. After Independence it was renamed as Raj Bhawan. The foundation stone of Nainital Raj Bhawan was laid on 27th April 1897 and the construction took two years to complete. It is built on European pattern and based on Gothic Architecture.
+
+The designers of Raj Bhawan (Nainital) were Architect Stevens and the Executive Engineer FOW Ortel. Burma teak has been used in conjunction with other versions of teak. Local stones have been used in its construction, with Ashler’s finishing. Some of the privileged occupants of the Raj Bhawan (Nainital) in the pre-Independence era include Sir Antony Mac Ddonald, Sir James, Sir John Misten, Sir Harcourt Butler, who were the then Governors of the United Provinces. In the post-Independence period, Smt. Sarojini Naidu, the first Governor of Uttar Pradesh, was the first occupant of this historic monument.
+
+Abutting the Raj Bhawan is 160 acres of forest land which has a number of faunal species, besides large number of floral varieties. The Raj Bhawan (Nainital) also has a Golf Course spread over 45 acres. The Golf Course, built in 1936, is one of the vintage golf courses in India, and is affiliated to the Indian Golf Union (IGU). Tourists can enjoy a game of golf after paying a small green fee.
     <</SYS>>
     
     ${userInput}[/INST]`;
